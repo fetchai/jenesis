775b433008c55fdb99da5be3ce0732bb5d9d2a00
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2022-07-14 14:23:16 +0100

    feat: add init and new cmd

--- src/haul/cmd/init.py
+++ src/haul/cmd/init.py
@@ -1,10 +1,45 @@
 import argparse
+import configparser
+import os
+import subprocess
 
 
-def run(_args: argparse.Namespace):
-    print('Implement me!')
+def run(args: argparse.Namespace):
+    user_name = subprocess.getoutput("git config user.name")
+    user_email = subprocess.getoutput("git config user.email")
+    authors = user_name + " <" + user_email + ">"
+
+    project_path = os.getcwd()
+    project_name = os.path.basename(project_path)
+
+    cli_path = project_path[: project_path.index("cli") + len("cli")]
+
+    if (
+        (os.path.join(cli_path, project_name) != project_path)
+        or (project_name == "src")
+        or (project_name == "tests")
+    ):
+        raise Exception("You must be in your project's directory")
+
+    config = configparser.ConfigParser()
+    config.add_section("project")
+    config.set("project", "name", project_name)
+    config.set("project", "authors", "[" + authors + "]")
+
+    config.add_section("profile.testing")
+    config.set("profile.testing", "network", "fetchai-dorado")
+
+    project = "contracts/.gitkeep"
+    path = os.path.join(project_path, "haul.toml")
+
+    try:
+        os.makedirs(project)
+        with open(path, "w") as configfile:
+            config.write(configfile)
+    except FileExistsError:
+        print("Project already initialized")
 
 
 def add_init_command(parser):
-    init_cmd = parser.add_parser('init')
+    init_cmd = parser.add_parser("init")
     init_cmd.set_defaults(handler=run)
--- src/haul/cmd/new.py
+++ src/haul/cmd/new.py
@@ -1,10 +1,38 @@
 import argparse
+import configparser
+import os
+import subprocess
 
 
-def run(_args: argparse.Namespace):
-    print('Implement me!')
+def run(args: argparse.Namespace):
+    user_name = subprocess.getoutput("git config user.name")
+    user_email = subprocess.getoutput("git config user.email")
+    authors = user_name + " <" + user_email + ">"
+
+    config = configparser.ConfigParser()
+    config.add_section("project")
+    config.set("project", "name", args.project)
+    config.set("project", "authors", "[" + authors + "]")
+
+    config.add_section("profile.testing")
+    config.set("profile.testing", "network", "fetchai-dorado")
+
+    current_path = os.getcwd()
+    cli_path = current_path[: current_path.index("cli") + len("cli")]
+    os.chdir(cli_path)
+
+    project = os.path.join(args.project, "contracts/.gitkeep")
+    path = os.path.join(cli_path, args.project, "haul.toml")
+
+    try:
+        os.makedirs(project)
+        with open(path, "w") as configfile:
+            config.write(configfile)
+    except FileExistsError:
+        print("Project already exists")
 
 
 def add_new_command(parser):
-    new_cmd = parser.add_parser('new')
+    new_cmd = parser.add_parser("new")
+    new_cmd.add_argument("-p", "--project", required=True, help="Project name")
     new_cmd.set_defaults(handler=run)
