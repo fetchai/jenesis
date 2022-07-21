d081ad63e71094dfe3288379819858859bbbe66a
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2022-07-21 10:36:44 +0100

    feat: code reformated, added test

--- src/haul/cmd/init.py
+++ src/haul/cmd/init.py
@@ -1,34 +1,13 @@
 import argparse
-import configparser
 import os
-import subprocess
+
+from haul.config import Config
 
 
 def run(_args: argparse.Namespace):
-    user_name = subprocess.getoutput("git config user.name")
-    user_email = subprocess.getoutput("git config user.email")
-    authors = user_name + " <" + user_email + ">"
 
-    project_path = os.getcwd()
-    project_name = os.path.basename(project_path)
-
-    config = configparser.ConfigParser()
-    config.add_section("project")
-    config.set("project", "name", project_name)
-    config.set("project", "authors", "[" + authors + "]")
-
-    config.add_section("profile.testing")
-    config.set("profile.testing", "network", "fetchai-dorado")
-
-    project = "contracts/.gitkeep"
-    path = os.path.join(project_path, "haul.toml")
-
-    try:
-        os.makedirs(project)
-        with open(path, "w", encoding="utf-8") as configfile:
-            config.write(configfile)
-    except FileExistsError:
-        print("Project already initialized")
+    project_name = os.path.basename(os.getcwd())
+    Config.create_project(project_name)
 
 
 def add_init_command(parser):
--- src/haul/cmd/new.py
+++ src/haul/cmd/new.py
@@ -1,37 +1,13 @@
 import argparse
-import configparser
-import os
-import subprocess
+
+from haul.config import Config
 
 
 def run(args: argparse.Namespace):
-    user_name = subprocess.getoutput("git config user.name")
-    user_email = subprocess.getoutput("git config user.email")
-    authors = user_name + " <" + user_email + ">"
-
-    config = configparser.ConfigParser()
-    config.add_section("project")
-    config.set("project", "name", args.project)
-    config.set("project", "authors", "[" + authors + "]")
-
-    config.add_section("profile.testing")
-    config.set("profile.testing", "network", "fetchai-dorado")
-
-    current_path = os.getcwd()
-
-    project = os.path.join(args.project, "contracts/.gitkeep")
-
-    path = os.path.join(current_path, args.project, "haul.toml")  # new
-
-    try:
-        os.makedirs(project)
-        with open(path, "w", encoding="utf-8") as configfile:
-            config.write(configfile)
-    except FileExistsError:
-        print("Project already exists")
+    Config.create_project(args.project)
 
 
 def add_new_command(parser):
     new_cmd = parser.add_parser("new")
-    new_cmd.add_argument("-p", "--project", required=True, help="Project name")
+    new_cmd.add_argument("project", metavar="NAME", help="Project name")
     new_cmd.set_defaults(handler=run)
