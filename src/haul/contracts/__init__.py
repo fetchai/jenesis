0b0910f772a6d71e8e88c6a63e4c3b52e802b6af
James Riehl <jrriehl@gmail.com>
2022-08-08 15:44:47 +0100

    refactor: separate contracts and deployments

--- src/haul/contracts/__init__.py
+++ src/haul/contracts/__init__.py
@@ -10,9 +10,16 @@ class Contract:
     name: str
     source_path: str
     binary_path: str
+    schema: dict
 
     def digest(self) -> Optional[str]:
         if not os.path.isfile(self.binary_path):
             return None
 
         return _compute_digest(self.binary_path).hex()
+
+    def functions(self):
+        return self.schema['properties']
+
+    def __repr__(self):
+        return self.name
