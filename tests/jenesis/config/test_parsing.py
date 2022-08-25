683d22d0a62fbd751215a42d5dd79423e3efa42f
James Riehl <jrriehl@gmail.com>
2022-08-25 11:18:45 +0100

    test: update network import

--- tests/jenesis/config/test_parsing.py
+++ tests/jenesis/config/test_parsing.py
@@ -5,7 +5,7 @@ import subprocess
 import pytest
 import toml
 from jenesis.config import Config, ConfigurationError
-from jenesis.contracts.networks import fetchai_testnet_config
+from jenesis.network import fetchai_testnet_config
 
 fail_parse_cases = (
     ({}, {}, r"unable to extract configuration string project\.name"),
