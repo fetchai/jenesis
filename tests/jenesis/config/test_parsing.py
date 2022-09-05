b81286b946189cdd84cd980ebb2fbbaea7afe5c0
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2022-09-05 13:44:56 +0100

    feat: minor test change

--- tests/jenesis/config/test_parsing.py
+++ tests/jenesis/config/test_parsing.py
@@ -177,7 +177,7 @@ def test_compile_contract():
     Config.add_contract(contract_root, template, contract_name, None)
 
     subprocess.run("jenesis compile", shell=True)
-    time.sleep(15)
+    time.sleep(35)
 
     compiled_contract = os.path.join(
         contract_root, "artifacts", contract_name + ".wasm"
