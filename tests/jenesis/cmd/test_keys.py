88b8b6ffb86436d057b6711b404c593142d84e91
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2022-09-30 16:22:40 +0100

    feat: test

--- tests/jenesis/cmd/test_keys.py
+++ tests/jenesis/cmd/test_keys.py
@@ -6,11 +6,12 @@ def test_keys():
     """Test key command"""
 
     subprocess.run("fetchd config keyring-backend test", shell=True)
-    
+
     key = "sample_key"
     subprocess.run("fetchd keys add " + key, shell=True)
 
-    key_address = subprocess.getoutput("fetchd keys show -a " + key)
+    #key_address = subprocess.getoutput("fetchd keys show -a " + key)
+    key_address = subprocess.getoutput("fetchd keys add " + key)
 
     #jenesis_key_address = subprocess.getoutput("jenesis keys show " + key)
 
