8b744282c2b7eb531688d372f02c868fb6abbf51
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2022-10-03 17:39:41 +0100

    feat: Unlock Keyring

--- tests/jenesis/cmd/test_keys.py
+++ tests/jenesis/cmd/test_keys.py
@@ -9,11 +9,11 @@ def test_keys():
 
     key = "zz"
 
-    x = subprocess.run("echo 'password' | gnome-keyring-daemon --unlock", shell=True, capture_output=True)  
+    #x = subprocess.run("echo 'password' | gnome-keyring-daemon --unlock", shell=True, capture_output=True)  
 
-    print( 'exit statusx:', x.returncode )
-    print( 'stdoutx:', x.stdout.decode() )
-    print( 'stderrx:', x.stderr.decode() )
+    #print( 'exit statusx:', x.returncode )
+    #print( 'stdoutx:', x.stdout.decode() )
+    #print( 'stderrx:', x.stderr.decode() )
 
     p = subprocess.run("fetchd keys add " + key, shell=True, capture_output=True)
 
